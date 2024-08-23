26c3d2379af4fd76e0a59909125f9ec8ae57c7fd
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2024-08-23 19:11:44 +0530

    update

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,20 +1,39 @@
-// LoginPage.js
 import React from 'react';
-import './LoginPage.css'; // Import the CSS file for styling
+import './LoginPage.css'; 
 
+
+import logo from './assets/logo.png'; 
 const LoginPage = () => {
+  const handleAdminPageClick = () => {
+    window.location.href = '/admin/dashboard'; 
+  };
+
+  const handleFacultyPageClick = () => {
+    window.location.href = '/faculty'; // Replace '/faculty' with your actual route for the Faculty Page
+  };
+
   return (
     <div className="login-page">
       <div className="login-container">
         <h2 className="welcome-text">Welcome Back!</h2>
         <img
-          src="logo.png" // Replace with your actual logo URL
+          src={logo}
           alt="Bannari Amman Institute of Technology"
           className="login-logo"
         />
         <hr className="divider" />
         <button className="google-sign-in">Google Sign In</button>
         <p className="sign-in-text">Sign in with your BIT account</p>
+      </div>
+
+
+      <div className="button-container">
+        <button className="admin-page-button" onClick={handleAdminPageClick}>
+          Go to Admin Page
+        </button>
+        <button className="faculty-page-button" onClick={handleFacultyPageClick}>
+          Go to Faculty Page
+        </button>
       </div>
     </div>
   );
---
+++ src/components/faculty/frontend/FacultyPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FacultyPage = () => {
+  return (
+    <div className="faculty-page">
+      <h1>Faculty Page</h1>
+    </div>
+  );
+};
+
+export default FacultyPage;
