c395e4f3b2081d1b1eea9d619e5af6ee1aa34da7
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2024-09-24 10:57:10 +0530

    small change

--- frontend/src/components/faculty/frontend/FacultyRequest.jsx
+++ frontend/src/components/faculty/frontend/FacultyRequest.jsx
@@ -4,11 +4,11 @@ import './FacultyRequest.css';
 
 const FacultyRequest = () => {
   const requestData = [
-    { subjectID: 'IT123', subjectName: 'DSA', semester: 'Sem 8', dueDate: '2024-09-15' },
-    { subjectID: 'CS123', subjectName: 'OS', semester: 'Sem 2', dueDate: '2024-09-20' },
-    { subjectID: 'CB123', subjectName: 'DAA', semester: 'Sem 4', dueDate: '2024-09-25' },
-    { subjectID: 'EC123', subjectName: 'EE', semester: 'Sem 1', dueDate: '2024-10-01' },
-    { subjectID: 'BT123', subjectName: 'BT', semester: 'Sem 8', dueDate: '2024-10-05' },
+    { subjectID: 'IT123', subjectName: 'DSA', semester: 'Sem 8', dueDate: '2024-09-15', daysPending: -9 },
+    { subjectID: 'CS123', subjectName: 'OS', semester: 'Sem 2', dueDate: '2024-09-20', daysPending: -4 },
+    { subjectID: 'CB123', subjectName: 'DAA', semester: 'Sem 4', dueDate: '2024-09-25',  daysPending: 1},
+    { subjectID: 'EC123', subjectName: 'EE', semester: 'Sem 1', dueDate: '2024-10-01', daysPending: 6 },
+    { subjectID: 'BT123', subjectName: 'BT', semester: 'Sem 8', dueDate: '2024-10-05', daysPending: 10 },
   ];
 
   const navigate = useNavigate();
@@ -28,6 +28,7 @@ const FacultyRequest = () => {
               <th>Subject Name</th>
               <th>Semester</th>
               <th>Due Date</th>
+              <th>Days Pending</th>
               <th>Action</th>
             </tr>
           </thead>
@@ -38,10 +39,11 @@ const FacultyRequest = () => {
                 <td>{request.subjectName}</td>
                 <td>{request.semester}</td>
                 <td>{request.dueDate}</td>
+                <td>{request.daysPending}</td>
                 <td>
                   <button
                     className="faculty-request-upload-button"
-                    onClick={() => handleUploadClick(request.dueDate)} // Pass dueDate to handler
+                    onClick={() => handleUploadClick(request.dueDate)}
                   >
                     Upload
                   </button>
